0ae44fd4dbad1e3dbb9e7d33db197ccad1ae7f62
MondaleFelix <mondalefelix@yahoo.com>
2020-06-12 12:27:49 -0700

    ground now scrolls

--- HoppyBunny/HoppyBunny/GameScene.swift
+++ HoppyBunny/HoppyBunny/GameScene.swift
@@ -6,14 +6,18 @@ import GameplayKit
 class GameScene: SKScene {
     
     var hero: SKSpriteNode!
+    var scrollLayer: SKNode!
+    
     var sinceTouch : CFTimeInterval = 0
     let fixedDelta: CFTimeInterval = 1.0 / 60.0 /* 60 FPS */
+    let scrollSpeed: CGFloat = 100
     
     override func didMove(to view: SKView) {
         /* Setup your scene here */
         
         // recursive node search for 'hero' (child of referenced node)
         hero = (self.childNode(withName: "//hero") as! SKSpriteNode)
+        scrollLayer = self.childNode(withName: "scrollLayer")
         
         // allows the hero to animate when it's in the GameScene
         hero.isPaused = false
@@ -52,6 +56,30 @@ class GameScene: SKScene {
 
         /* Update last touch timer */
         sinceTouch += fixedDelta
-         
+
+        scrollWorld()
+    }
+    
+    
+    func scrollWorld() {
+        /* Scroll World */
+        scrollLayer.position.x -= scrollSpeed * CGFloat(fixedDelta)
+        
+        for ground in scrollLayer.children as! [SKSpriteNode] {
+
+        /* Get ground node position, convert node position to scene space */
+        let groundPosition = scrollLayer.convert(ground.position, to: self)
+
+        /* Check if ground sprite has left the scene */
+        if groundPosition.x <= -ground.size.width / 2 {
+
+            /* Reposition ground sprite to the second starting position */
+            let newPosition = CGPoint(x: (self.size.width / 2) + ground.size.width, y: groundPosition.y)
+
+            /* Convert new node position back to scroll layer space */
+            ground.position = self.convert(newPosition, to: scrollLayer)
+        }
+        }
+
     }
 }
